807cf5b85c90b64c5dc57c49db008f6005b5d376
Kennethram15 <129108279+Kennethram15@users.noreply.github.com>
2026-07-28 18:30:50 -0600

    Detalles
    Correcciones y un CRUD para usuarios

--- farmacia-backend/routes/usuariosRoutes.js
+++ farmacia-backend/routes/usuariosRoutes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const controller = require("../controllers/usuariosController");
+const { requiereRol } = require("../middlewares/auth");
 
 const router = Router();
 
 router.get("/", controller.getAll);
 router.get("/:id", controller.getById);
-router.post("/", controller.create);
-router.put("/:id", controller.update);
-router.delete("/:id", controller.remove);
+router.post("/", requiereRol('Administrador'), controller.create);
+router.put("/:id", requiereRol('Administrador'), controller.update);
+router.delete("/:id", requiereRol('Administrador'), controller.remove);
 
 module.exports = router;
